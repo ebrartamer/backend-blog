498278f3793b1b5fddd95f4548fad1ce0eeb95cb
Ebrar Tamer <ebrartamer@Ebrar-Air.hgw.local>
2024-12-08 15:22:46 +0300

    Feat : Blog Controller fix

---
+++ controllers/blogModel.js
@@ -0,0 +1,30 @@
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  replies: [{
+    content: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}); 
